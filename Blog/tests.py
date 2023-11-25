11766bbf1122f41680861bd36a9d4a936a07a2f2
lauramorales <ma.lauramorales@gmail.com>
2023-11-25 17:20:19 -0300

    tests

--- Blog/tests.py
+++ Blog/tests.py
@@ -2,3 +2,23 @@ from django.test import TestCase
 
 # Create your tests here.
 
+
+class ArticuloTests(TestCase):
+    """En esta clase van todas las pruebas del modelo Articulo."""
+
+    def test_articulo_create(self):
+        # caso uso esperado
+        articulo = articulo(titulo="Titulo", subtitulo="subtitulo")
+        articulo.save()
+
+        # Compruebo que el articulo fue creado y la info fue guardada bien
+        self.assertEqual(articulo.objects.count(), 1)
+        self.assertEqual(articulo.nombre, "Titulo")
+        self.assertEqual(articulo.subtitulo, "subtitulo")
+
+    def test_articulo_str(self):
+        articulo = articulo(nombre="Python", subtitulo="Django")
+        articulo.save()
+
+        # Compruebo el str funciona como se desea
+        self.assertEqual(articulo.__str__(), "Python (Django)")
